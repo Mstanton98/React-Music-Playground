df28ff5d3a49124045c84fb7457f1482d999870d
Mstanton98 <mstanton98@gmail.com>
2016-12-07 13:55:20 -0800

    initial commit/file structure/brunch setup

---
+++ app/components/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ReactMusic from 'react-music';
+
+class React-Music-Playground extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    };
+  }
+
+  render() {
+    return (
+      <Song tempo="160">
+        
+      </Song>
+
+    );
+  }
+}
---
+++ brunch-config.js
@@ -0,0 +1,34 @@
+'use strict';
+
+module.exports = {
+  files: {
+    javascripts: {
+      joinTo: {
+        'app.js': /^app//,
+        'vendor.js': /^node_modules//
+      }
+    },
+
+    stylesheets: {
+      joinTo: {
+        'app.css': /^app//,
+        'vendor.css': /^node_modules//
+      }
+    }
+  },
+
+  npm: {
+    styles: {
+    }
+  },
+
+  plugins: {
+    babel: {
+      presets: ['es2015', 'react']
+    }
+  },
+
+  server: {
+    port: Number.parseInt(process.env.PORT) || 8000
+  }
+};
